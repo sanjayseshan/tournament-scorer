324a6a8f33442f4e9865c0cd428731a990d1108b
Sanjay Seshan <sanjay.seshan@outlook.com>
2018-08-07 19:52:57 -0400

    old php version

---
+++ sanjay.seshan@outlook.com
@@ -0,0 +1,2 @@
+921143e68097280d02943b251f36ab40
+Tournament Director
---
+++ scorer/languages.js
@@ -0,0 +1,257 @@
+var monthNames = ["","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+improvement = "Improvement"
+saveText = "Save"
+savescoreText="Save Score"
+loadsaveText="Load Save:"
+deletesaveText="Delete Saves"
+importsaveText="Import Save"
+exportsaveText="Export Save"
+scorerText = "Scorer"
+timersText = "Timers"
+savesText = "Saves"
+startText = "Start"
+stopText = "Stop"
+improvementText = ""
+  title = "INTO ORBIT SCORER"
+
+//resetText = "Reset"
+
+
+if (language == 'pt-br') {
+var monthNames = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro" , "Dezembro"];
+improvement = "Melhoria"
+saveText = "Salvar"
+savescoreText = "Save Score"
+loadsaveText = "Load Save"
+deletesaveText = "Excluir salva"
+importsaveText = "Importar Salvar"
+exportsaveText = "Exportar Salvar"
+scorerText = "Scorer"
+timersText = "Temporizadores"
+savesText = "Salva"
+startText = "Iniciar"
+stopText = "Stop"
+  yes = 'Sim'
+  no = 'Não'
+  pointsText = 'Pontos'
+  resetText = 'Reiniciar'
+  partly = "Parcialmente"
+  completely = "Completamente"
+
+
+
+} else if (language == 'de') {
+var monthnames = [ "", "Januar", "Februar", "März", "April", "Mai", "June", "Juli", "August", "September", "October", "November" , "Dezember"];
+improvement = "Verbesserung"
+saveText = "Speichern"
+savescoreText = "Ergebnis speichern"
+loadsaveText = "Load Save"
+deletesaveText = "Löschen speichert"
+importsaveText = "Import speichern"
+exportsaveText = "Export speichern"
+scorerText = "Punkterechner"
+timersText = "Timer"
+savesText = "Saves"
+startText = "Start"
+stopText = "Stop"
+  yes = 'Ja'
+  no = 'Nein'
+  pointsText = 'Punkte'
+  resetText = 'Zurücksetzen'
+  partly = "Teilweise"
+  completely = "Komplett"
+
+} else if (language == 'nl') {
+
+  yes = 'Ja'
+  no = 'Nee'
+  pointsText = 'Punten'
+  resetText = 'Resetten'
+  partly = "Gedeeltelijk"
+  completely = "Volledig"
+
+var monthNames = ["", "Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November" , "December"];
+improvementText = "verbetering"
+saveText = "Opslaan"
+savescoreText = "Score opslaan"
+loadsaveText = "Load Save"
+deletesaveText = "Delete Save"
+importsaveText = "Import Save"
+exportsaveText = "Export Save"
+scorerText = "Scorer"
+timersText = "Timers"
+savesText = "Saves"
+startText = "Start"
+stopText = "Stop"
+
+} else if (language == 'es') {
+  yes = 'Si'
+  no = 'No'
+  pointsText = 'Puntos'
+  resetText = 'Reiniciar'
+  partly = "Parcialmente"
+  completely = "Completamente"
+
+var monthNames  = [ "", "Enero", "Febrero", "Marcha", "Abril", "puede", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre" , "Diciembre"];
+improvementText = "Mejora"
+saveText = "Guardar"
+savescoreText = "Guardar Puntuación"
+loadsaveText = "Guardar carga"
+deletesaveText = "Eliminar Salva"
+importsaveText = "Importar Guardar"
+exportsaveText = "Exportar Guardar"
+scorerText = "Goleador"
+timersText = "Temporizadores"
+saveesText = "Guarda"
+startText = "Inicio"
+stopText = "Detener"
+
+
+} else if (language == 'sk') {
+  yes = 'Áno'
+  no = 'Nie'
+  pointsText = 'Skóre'
+  resetText = 'Reset'
+  partly = "Čiastočne"
+  completely = "Celkom"
+var monthNames = ["", "január", "február", "marec", "apríl", "máj", "jún", "júl", "august", "september" , "December"];
+improvementText = "Improvement"
+saveText = "Uložiť"
+savescoreText = "Uložiť skóre"
+loadaveText = "Načítať uložiť"
+deletesaveText = "Odstrániť šetrí"
+importaveText = "Importovať"
+exportaveText = "Exportovať uloženie"
+scorerText = "Scorer"
+timersText = "Časovače"
+savesText = "Uloží"
+startText = "Štart"
+stopText = "Stop"
+
+
+} else if (language == 'hu') {
+  yes = 'Igen'
+  no = 'Nem'
+  pointsText = 'Pontok'
+  resetText = 'Nullázás'
+  partly = "Részben"
+  completely = "Teljesen"
+
+var monthNames = ["", "január", "február", "március", "április", "május", "júniusi", "július", "augusztus", "szeptember" , "December"];
+improvementText = "Javítás"
+saveText = "Mentés"
+savescoreText = "Pontszám mentése"
+loadsaveText = "Betöltés mentése"
+deletesaveText = "Mentés törlése"
+importaveText = "Mentés importálása"
+exportálásText = "Mentés exportálása"
+scorerText = "Gólszerző"
+timersText = "Időzítők"
+savesText = "Elmenti"
+startText = "Start"
+stopText = "Stop"
+
+
+
+} else if (language == 'el') {
+  yes = "Ναι"
+  no = "Όχι"
+  partly = "Μερικώς"
+  completely = "Εντελώς"
+  pointsText = 'Πόντοι'
+  resetText = 'Επαναφορά'
+
+var monthNames = [ "", "Ιανουάριος", "Φεβρουάριος", "Μάρτιος", "Απρίλιος", "Μαΐου", "Ιούνιος", "Ιούλιος", "Αύγουστος", "Σεπτέμβρης", "Οκτώβριος", "Νοέμβριος" , "Δεκέμβριος"]
+
+improvementText = "Βελτίωση"
+saveText = "Αποθήκευση"
+savescoreText = "Αποθήκευση σκορ"
+loadaveText = "Φόρτωση Αποθήκευσης"
+deletesaveText = "Διαγραφή Αποθηκεύει"
+importsaveText = "Εισαγωγή Αποθήκευσης"
+exportsaveText = "Εξαγωγή Αποθήκευσης"
+scorerText = "Σκοπευτής"
+timersText = "Χρονοδιακόπτες"
+savesText = "Αποθηκεύει"
+startText = "Έναρξη"
+stopText = "Διακοπή"
+
+
+
+
+} else if (language == 'fr') {
+  yes = "Oui"
+  no = "Non"
+  partly = "Partiellement"
+  completely = "Complètement"
+  pointsText = "Points"
+  resetText = "Redémarrer"
+  
+var monthNames = ["", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre" , "Décembre"];
+improvementText = "Amélioration"
+saveText = "Enregistrer"
+savescoreText = "Enregistrer le score"
+loadsaveText = "Charger Enregistrer"
+deleteaveText = "Supprimer les sauvegardes"
+importsaveText = "Importer enregistrer"
+exportsaveText = "Exporter enregistrer"
+scorerText = "Scorer"
+timersText = "Timers"
+saveText = "Sauve"
+startText = "Démarrer"
+stopText = "Stop"
+
+
+
+} else if (language == 'he') {
+  yes = "כן"
+  no = "לא"
+  partly = "חלקית"
+  completely = "לגמרי"
+  pointsText = 'נקודות'
+  resetText = 'אפס'
+
+
+var monthNames = [ "", "ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי", "אוגוסט", "ספטמבר", "אוקטובר", "נובמבר" , "דצמבר"];
+improvementText = "שיפור"
+saveText = "שמור"
+savescoreText = "שמור ציון"
+loadsaveText = "טען שמירה"
+deletesaveText = "מחק שמירת"
+importsaveText = "ייבוא שמור"
+exportsaveText = "ייצוא שמירה"
+scorerText = "מלך השערים"
+timersText = "טיימרים"
+savesText = "שומר"
+startText = "התחל"
+stopText = "עצור"
+
+
+
+
+
+} else {
+
+  title = "INTO ORBIT SCORER"
+  yes = "Yes"
+  no = "No"
+  partly = "Partly"
+  completely = "Completely"
+  pointsText = "Points"
+  resetText = "Reset"
+var monthNames = ["","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+improvementText = "Improvement"
+saveText = "Save"
+savescoreText="Save Score"
+loadsaveText="Load Save:"
+deletesaveText="Delete Saves"
+importsaveText="Import Save"
+exportsaveText="Export Save"
+scorerText = "Scorer"
+timersText = "Timers"
+savesText = "Saves"
+startText = "Start"
+stopText = "Stop"
+
+}
+improvement= improvementText
